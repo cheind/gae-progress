17f29483d171f30cda52903b556bb1d8bd6ecb3e
Christoph Heindl <christoph.heindl@gmail.com>
2016-01-13 14:37:59 +0100

    Updated client to new API names

--- client/progress.py
+++ client/progress.py
@@ -11,7 +11,7 @@ import inspect
 # Install with pip install --upgrade google-api-python-client
 from apiclient.discovery import build
 
-def createService(args):
+def createProgressService(args):
     # Build a service object for interacting with the API.
     apiRoot = urlparse.urljoin(args.url, '_ah/api')
     api = 'progressApi'
@@ -20,17 +20,20 @@ def createService(args):
     return build(api, version, discoveryServiceUrl=discovery_url, cache_discovery=False)
 
 def listProgresses(args):
-    service = createService(args)
+    progress = createProgressService(args)
 
     result = []
     page_token = None
-    param = {}
-    param['order'] = '-created'
+    param = {
+        'order': '-lastUpdated',
+        'apikey': args.key
+    }
+
     while True:
         if page_token:
             param['pageToken'] = page_token
 
-        response = service.progress().list(**param).execute()
+        response = progress.list(**param).execute()
 
         result.extend(response['items'])
         page_token = response.get('nextPageToken')
@@ -40,16 +43,16 @@ def listProgresses(args):
     pprint.pprint(result)
 
 def createProgress(args):
-    service = createService(args)
-    response = service.progress().create(body={'title':args.name, 'description':args.desc, 'apikey':args.key}).execute()
+    progress = createProgressService(args)
+    response = progress.create(body={'title':args.name, 'description':args.desc, 'apikey':args.key}).execute()
     pprint.pprint(response)
 
 def simulateProgressUpdate(args):
-    service = createService(args)
+    progress = createProgressService(args)
     param = {'id':args.id, 'apikey':args.key}
     for x in range(0, 100):
         param['progress'] = x
-        response = service.progress().update(body=param).execute()
+        response = progress.update(body=param).execute()
         print '.',
         time.sleep(0.5)
 
@@ -71,10 +74,10 @@ def main():
      parserSimulateProgress.add_argument('--key', required=True)
      parserSimulateProgress.set_defaults(func=simulateProgressUpdate)
 
-     parserSimulateProgress = subparsers.add_parser('list')
-     parserSimulateProgress.add_argument('--url', default='http://localhost:8080')
-     parserSimulateProgress.add_argument('--key', required=True)
-     parserSimulateProgress.set_defaults(func=listProgresses)
+     parserListProgresses = subparsers.add_parser('list')
+     parserListProgresses.add_argument('--url', default='http://localhost:8080')
+     parserListProgresses.add_argument('--key', required=True)
+     parserListProgresses.set_defaults(func=listProgresses)
 
      args = parser.parse_args()
      args.func(args)
